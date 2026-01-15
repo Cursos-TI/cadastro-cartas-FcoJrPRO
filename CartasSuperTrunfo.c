95014f66e461805f8562edb1265cfb75c12b8329
Francisco Junior <202511006313@alunos.estacio.br>
2026-01-15 20:31:45 +0000

    inclui %.2f para facilitar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -85,8 +85,8 @@ int main() {
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
-    printf("Área: %f km²\n", area1); 
-    printf("PIB: %f bilhões de reais\n", pib1);
+    printf("Área: %.2f km²\n", area1); 
+    printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
     // --- EXIBIÇÃO DA CARTA 2 ---
@@ -95,8 +95,8 @@ int main() {
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("População: %d\n", populacao2);
-    printf("Área: %f km²\n", area2);
-    printf("PIB: %f bilhões de reais\n", pib2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
     return 0;
